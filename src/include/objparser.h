780105b5c33066173ce22e158ccb6ec893b11097
Nathan <nathoulegrand0@gmail.com>
2024-07-28 17:21:57 +0200

    Rolled own simple .obj parser, fix #3

---
+++ src/include/objparser.h
@@ -0,0 +1,54 @@
+#ifndef __OBJPARSER_H_
+#define __OBJPARSER_H_
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum {
+    FACETYPE_UNKNOWN = 0,  // Unknown format or empty object
+    FACETYPE_V       = 1,  // Line of the form : f v1 v2 v3 ...
+    FACETYPE_VT      = 2,  // Line of the form : f v1/t1 v2/t2 v3/t3 ...
+    FACETYPE_VN      = 3,  // Line of the form : f v1//n1 v2//n2 v3//n3 ...
+    FACETYPE_VTN     = 4   // Line of the form : f v1/t1/n1 v2/t2/n2 v3/t3/n3 ...
+} FaceType_t;
+
+/* Represent an object stored in .obj format.
+ * Note that we loose 5 padding bytes for the alignment of the structure.
+ * This is fine as we will likely not have a great number of such structure.
+ *
+ * xf, v and f form the CSR representation of the face-vertex adjacency matrix.
+ */
+typedef struct {
+    uint8_t dv;     // Dimension of vertices
+    uint8_t dt;     // Dimension of texture
+    FaceType_t ft;  // Type of the face
+    // Size of lists
+    uint64_t nv;    // Lenght of vertices
+    uint64_t nf;    // Number of faces
+    uint64_t nt;    // Number of texture
+    uint64_t nn;    // Number of normals
+    // Index in lists
+    uint64_t* fx;   // Face index. Starting index of face i in fvx/ftx/fnx
+    int64_t* fvx;   // faces vertex index. Size = xf[nf]
+    int64_t* ftx;   // face textures index. Size = xf[nf]
+    int64_t* fnx;   // face normals index. Size = xf[nf]
+    // Lists
+    double* v;      // Vertices. Size = nv*dv
+    double* t;      // texture. Size = nt*dt
+    double* n;      // Normals. Size = nn*3
+} OBJModel_t;
+OBJModel_t* OBJModel_init(void);
+void OBJModel_destroy(OBJModel_t* obj);
+
+/* Read a .obj file and put it in obj.
+ * Warning: the pointers in @obj have to be either NULL or malloc'd.
+ *          i.e. @obj has to be allocated using C11 "Object obj = {0}" notation
+ *               or using the OBJModel_init() function.
+ *          if they are NULL they will be malloc'd 
+ *          if they are malloc'd they will be freed and malloc'd
+ */
+bool OBJModel_read_file(OBJModel_t* obj, char const* filename);
+
+#endif // __OBJPARSER_H_
---
+++ src/objparser.c
@@ -0,0 +1,245 @@
+#include "include/objparser.h"
+#include <stdio.h>
+#include <string.h>
+
+OBJModel_t* OBJModel_init(void){
+    OBJModel_t* out = calloc(1, sizeof(OBJModel_t));
+    out->ft = FACETYPE_UNKNOWN;
+
+    out->fx = NULL;
+    out->fvx = NULL;
+    out->ftx = NULL;
+    out->fnx = NULL;
+
+    out->v = NULL;
+    out->t = NULL;
+    out->n = NULL;
+
+    return out;
+}
+
+void OBJModel_destroy(OBJModel_t *obj){
+    if (obj) {
+        if (obj->fx) free(obj->fx);
+        if (obj->fvx) free(obj->fvx);
+        if (obj->ftx) free(obj->ftx);
+        if (obj->fnx) free(obj->fnx);
+        if (obj->v) free(obj->v);
+        if (obj->t) free(obj->t);
+        if (obj->n) free(obj->n);
+        memset(obj, 0, sizeof(*obj));
+    }
+}
+
+bool OBJModel_read_file(OBJModel_t *obj, const char *filename){
+    if (!obj){
+        fprintf(stderr, "Tried to read a .obj file into a NULL pointer\n");
+        return false;
+    }
+
+    FILE* in = fopen(filename, "r");
+    if (!in) {
+        fprintf(stderr, "Cannot open file '%s'\n", filename);
+        return false;
+    }
+
+    uint8_t  dv  = 0;
+    uint8_t  dt  = 0;
+    uint64_t nv = 0;
+    uint64_t nf = 0;
+    uint64_t nt = 0;
+    uint64_t nn = 0;
+    uint64_t nfv = 0; // Final size of f (= xf[nf])
+    FaceType_t ft = FACETYPE_UNKNOWN;
+
+    // First pass for vertex and face count
+    char* line = NULL;
+    size_t linesize = 0;
+    size_t lineread = 0;
+    // TODO: change strncmpt for a switch case. The match of the switch 
+    // should be a unique number computed on the 2 first character of the line
+    while (getline(&line, &linesize, in) >= 0){
+        lineread++;
+
+        if (!strncmp("#", line, 1)) continue; 
+        if (!strncmp("\n", line, 1)) continue; 
+                                               
+        if (!strncmp("v ", line, 2)){
+            nv++;
+            // Only check the dimension on the first vertex.
+            // This is not robust : if a subsequent vertex ...
+            //   1) has more dimensions : the error goes unnoticed as we only read the first d dimension
+            //   2) has less dimensions : this will cause an error during parsing because we try to read more tokens
+            if (!dv){
+                strtok(line, " ");
+                while (strtok(NULL, " ")) dv++;
+            }
+
+        } 
+        else if (!strncmp("f ", line, 2)){
+            nf++;
+            char* tok = strtok(line, " ");
+            while ((tok = strtok(NULL, " "))) {
+                nfv++;
+                if (ft == FACETYPE_UNKNOWN){
+                    int a,b,c; // just trash variable
+                    if (sscanf(tok, "%d/%d/%d", &a, &b, &c) == 3) ft = FACETYPE_VTN;
+                    else if (sscanf(tok, "%d/%d", &a, &b) == 2) ft = FACETYPE_VT;
+                    else if (sscanf(tok, "%d//%d", &a, &b) == 2) ft = FACETYPE_VN;
+                    else if (sscanf(tok, "%d", &a) == 1) ft = FACETYPE_V;
+                    else {
+                        fprintf(stderr, 
+                                "Wrong face data : each vertex data should be of the form \n"
+                                "\t v1 ==> only vertex data\n"
+                                "\t v1/t1 ==> vertex + texture data\n"
+                                "\t v1/t1/n1 ==> vertex + texture + normal data\n"
+                                "\t v1//n1 ==> vertex + norma data\n"
+                                "Obtained instead : %s\n",
+                                tok);
+                        return false;
+                    }
+                }
+            }
+        } 
+        else if (!strncmp("vt", line, 2)){
+            nt++;
+            if (!dt){
+                strtok(line, " ");
+                while (strtok(NULL, " ")) dt++;
+            }
+        }
+        else if (!strncmp("vn", line, 2)){
+            nn++;
+        }
+        else {
+            char buf[3] = {line[0], line[1], '\0'};
+            fprintf(stderr, "Ignoring unsupported .obj element '%s' on line %zu\n", buf, lineread);
+        }
+    }
+
+    // Reset file cursor
+    rewind(in);
+    uint64_t* fx = malloc(sizeof(uint64_t) * (nf+1));
+    int64_t*  fvx  = malloc(sizeof(int64_t) * nfv);
+    int64_t*  ftx  = NULL;
+    int64_t*  fnx  = NULL;
+
+    double*   v  = malloc(sizeof(double) * nv * dv);
+    double*   t = NULL;
+    double*   n = NULL;
+    if (ft == FACETYPE_VTN || ft == FACETYPE_VT) {
+        t = malloc(sizeof(double) * nt * dt);
+        ftx = malloc(sizeof(int64_t) * nfv);
+    }
+    if (ft == FACETYPE_VTN || ft == FACETYPE_VN) {
+        n = malloc(sizeof(double) * nn * 3);
+        fnx = malloc(sizeof(int64_t) * nfv);
+    }
+
+    nv = 0;
+    nf = 0;
+    nt = 0;
+    nn = 0;
+    nfv = 0; 
+
+    fx[0] = 0;
+
+    while (getline(&line, &linesize, in) >= 0){
+        if (!strncmp("#", line, 1)) continue; 
+        if (!strncmp("\n", line, 1)) continue; 
+                                               
+        if (!strncmp("v ", line, 2)){
+            // Only check the dimension on the first vertex.
+            // This is not robust : if a subsequent vertex ...
+            //   1) has more dimensions : the error goes unnoticed as we only read the first d dimension
+            //   2) has less dimensions : this will cause an error during parsing because we try to read more tokens
+            char* tok = strtok(line, " ");
+            for (int32_t i = 0; i < dv; i++){
+                tok = strtok(NULL, " ");
+                v[nv*dv+i] = strtod(tok, NULL);
+            }
+            nv++;
+        } 
+        else if (!strncmp("vt", line, 2)){
+            char* tok = strtok(line, " ");
+            for (int32_t i = 0; i < dt; i++){
+                tok = strtok(NULL, " ");
+                t[nt*dt+i] = strtod(tok, NULL);
+            }
+            nt++;
+        }
+        else if (!strncmp("vn", line, 2)){
+            char* tok = strtok(line, " ");
+            for (int32_t i = 0; i < 3; i++){
+                tok = strtok(NULL, " ");
+                n[nn*3+i] = strtod(tok, NULL);
+            }
+            nn++;
+        }
+        else if (!strncmp("f ", line, 2)){
+            char* tok = strtok(line, " ");
+            while ((tok = strtok(NULL, " "))) {
+                switch (ft){
+                    case FACETYPE_V:
+                        if (sscanf(tok, "%ld", &fvx[nfv]) != 1) goto cleanup_on_parse_error;
+                        fvx[nfv]--;
+                        break;
+                    case FACETYPE_VT:
+                        if (sscanf(tok, "%ld/%ld", &fvx[nfv], &ftx[nfv]) != 2) goto cleanup_on_parse_error;
+                        fvx[nfv]--; ftx[nfv]--;
+                        break;
+                    case FACETYPE_VN:
+                        if (sscanf(tok, "%ld//%ld", &fvx[nfv], &fnx[nfv]) != 2) goto cleanup_on_parse_error;
+                        fvx[nfv]--; fnx[nfv]--;
+                        break;
+                    case FACETYPE_VTN:
+                        if (sscanf(tok, "%ld/%ld/%ld", &fvx[nfv], &ftx[nfv], &fnx[nfv]) != 3) goto cleanup_on_parse_error;
+                        fvx[nfv]--; ftx[nfv]--; fnx[nfv]--;
+                        break;
+                    default:
+                        break;
+                }
+                nfv++;
+            }
+            nf++;
+            fx[nf] = nfv;
+        } 
+    }
+    free(line);
+    fprintf(stderr, "Successfully read from '%s': \n"
+                    "\t%zu vertices of dimension %d\n"
+                    "\t%zu faces of type %d\n"
+                    "\t%zu texture of dimension %d\n"
+                    "\t%zu normals\n",
+                    filename, nv, dv, nf, ft, nt, dt, nn);
+
+    // Overwrite current object
+    OBJModel_destroy(obj); 
+    obj->dv  = dv;
+    obj->dt  = dt;
+    obj->ft = ft;
+
+    obj->nv = nv;
+    obj->nf = nf;
+
+    obj->fx = fx;
+    obj->fvx  = fvx;
+    obj->ftx  = ftx;
+    obj->fnx  = fnx;
+    obj->v  = v;
+    obj->t  = t;
+    obj->n  = n;
+
+    return true;
+
+    cleanup_on_parse_error:
+        free(fx);
+        free(fvx);
+        free(ftx);
+        free(fnx);
+        free(v);
+        free(t);
+        free(n);
+        fprintf(stderr, "Unexpected token encountered while parsing faces, aborting function\n");
+        return false;
+}
